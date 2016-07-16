53363022c032b9ab499e5aefb0779bb705b05d27
Geomart Brenth Abong <n2txheen@gmail.com>
2016-07-16 16:24:44 +0800

    Implemented GettersUnit and its test. Implemented ClassEndUnit.

--- src/main/java/org/lambda_n2t/codegenerator/CodeGenerator/CodeGeneratorUnit/JavaCodeGeneratorUnit/ClassEndUnit.java
+++ src/main/java/org/lambda_n2t/codegenerator/CodeGenerator/CodeGeneratorUnit/JavaCodeGeneratorUnit/ClassEndUnit.java
@@ -1,15 +1,15 @@
 package org.lambda_n2t.codegenerator.CodeGenerator.CodeGeneratorUnit.JavaCodeGeneratorUnit;
+
 import org.lambda_n2t.codegenerator.CodeGenerator.CodeGeneratorUnit.CodeGeneratorUnit;
 
 import java.util.Map;
 
 /**
- * Created by Geomart Brenth Abong on 7/15/2016.
+ * Created by Geomart Brenth Abong on 7/16/2016.
  */
-
-public class SpaceUnit implements CodeGeneratorUnit{
+public class ClassEndUnit implements CodeGeneratorUnit {
     public String getGeneratedCode() {
-        return "\n";
+        return "\t}\n";
     }
 
     public void generate(Map map) {
