e8daf3eb67522c13de8e3362cf4b2ab91e0b9106
Geomart Brenth Abong <n2txheen@gmail.com>
2016-07-19 23:33:11 +0800

    Removed  RandomClass

--- RandomClass.java
+++
@@ -1,9 +0,0 @@
-//----------------------------------------------|
-//
-// Programmer: Geomart Brenth Abong
-// Date Created: 2016-07-19
-//
-//----------------------------------------------|
-
-public class RandomClass {
-}
