bcad64a8fb0ba3edd868ac09876d1c6e97daf143
Geomart Brenth Abong <n2txheen@gmail.com>
2016-07-19 23:32:14 +0800

    Just to remove something

--- RandomClass.java
+++ RandomClass.java
@@ -1,45 +1,9 @@
 //----------------------------------------------|
 //
 // Programmer: Geomart Brenth Abong
-// Date Created: 2016-07-18
+// Date Created: 2016-07-19
 //
 //----------------------------------------------|
 
 public class RandomClass {
-
-	private String str;
-	private int n;
-	private boolean b;
-
-    //----------------------------------------------|
-    // Setter(s)
-    //----------------------------------------------|
-
-	public void setStr(String str) {
-		this.str = str;
-	}
-
-	public void setN(int n) {
-		this.n = n;
-	}
-
-	public void setB(boolean b) {
-		this.b = b;
-	}
-
-    //----------------------------------------------|
-    // Getter(s)
-    //----------------------------------------------|
-
-	public String getStr() {
-		return this.str;
-	}
-
-	public int getN() {
-		return this.n;
-	}
-
-	public boolean getB() {
-		return this.b;
-	}
 }
